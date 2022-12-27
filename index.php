32fa708647263c3a1b52a2d8f45f57ec39cf12f9
Antognioni <121345652+Antognioni@users.noreply.github.com>
2022-12-27 18:14:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,8 @@ echo "<h2>Rodando PHP 8</h2>";
 echo "<h3>Este repositório, é pra testar e rodar, pra testar</h3>";
 echo "<h4>Att, Tércio</h4>";
 
+echo " <h1>Fala meu querido</h1> ";
+echo " <h2>Excelente</h2> ";
+echo " <h3>já vou dar uma olhada no laravel, talvez começar a criar um template</h3> ";
+echo " <h4>se eu puder ajudar em alguma coisa com o banco</h4> ";
 ?>
