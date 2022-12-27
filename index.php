dfe8060fa14d98ac491d85db98d28d51aa9b5cb9
Tércio Cretton <cretton@gmail.com>
2022-12-27 18:30:19 -0300

    alterei titulo

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Hello Tony</h1>";
+echo "<h1>Sistema TPE - Repositório de testes</h1>";
 echo "<h2>Rodando PHP 8</h2>";
 echo "<h3>Este repositório, é pra testar e rodar, pra testar</h3>";
 echo "<h4>Att, Tércio</h4>";
