fb27a62fd52a4c472017cb7ffe130c51a856e19e
Tércio Cretton <cretton@gmail.com>
2022-12-27 17:46:11 -0300

    ok

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-echo "<h1>Hello World!</h1>";
+echo "<h1>Hello Tony</h1>";
+echo "<h2>Rodando PHP 8</h2>";
+echo "<h3>Este repositório, é pra testar e rodar, pra testar</h3>";
+echo "<h4>Att, Tércio</h4>";
+
 ?>
